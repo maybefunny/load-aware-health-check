715080f6b6a454ea19503cd0f8b2cb3385a1c47d
darfi sultoni <darfisultoni@gmail.com>
2021-08-19 13:19:50 +0700

    chore: clients get data direct from prometheus

--- client.py
+++ client.py
@@ -1,48 +1,36 @@
-import socket
 import sys
 import time
-import threading
-
-class Client:
-    lsock = None
-    sock = None
-    stat = None
-    load = None
-    addr = None
-    port = None
-    def __init__(self, addr, port) -> None:
-        self.addr = addr
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        client_thread = threading.Thread(target=self.client, daemon=True)        
-        client_thread.start()
-        client_thread.join()
-
-    def client(self) -> None:
-        self.sock.settimeout(3)
-        self.sock.connect((self.addr, self.port))
-        while(True):
-            self.sock.sendall(b'5')
-            data = self.sock.recv(1024)
-
-            self.stat = data.decode()
-            # f = open('index.html', 'w')
-            # f.writelines(self.stat)
-            # f.close()
-            time.sleep(1)
+import requests
+import json
 
 def main():
-    # get server address from user
-    addr = input("server address (None): ")
-    port = 5054
-    newport = input("server address (5054): ")
+    # get host address from user
+    addr = input("your address (None): ")
     if(addr == ''):
         print('server address is needed!')
         sys.exit(0)
-    if(newport != ''):
-        port = int(newport)
-    client = Client(addr, port)
+    query = 'bottomk(1, avg by (instance) (100 - rate(node_cpu_seconds_total{mode="idle"}[30s]) * 100))'
+    url = 'http://10.199.2.120:9090/api/v1/query?query=' + query
+    while(True):
+        r = requests.get(url)
+        try:
+            if(r.status_code == 200):
+                res = json.loads(r.text)
+                data = []
+                stat = ''
+                for item in res["data"]["result"]:
+                    data.append(item["metric"]["instance"][:-5])
+                if(addr in data):
+                    stat = 'true'
+                else:
+                    stat = 'false'
+                print(repr(data))
+                f = open('stat.html', 'w')
+                f.writelines(stat)
+                f.close()
+        except:
+            pass
+        time.sleep(10)
 
 if __name__ == "__main__":
     try:

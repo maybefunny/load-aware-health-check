2c8fd2dc79be46e01de652efe2d29c2f4581d7f6
darfi sultoni <darfisultoni@gmail.com>
2021-08-27 09:52:49 +0700

    ok

--- load.py
+++ load.py
@@ -1,20 +1,39 @@
 import time
 import psutil
 
-app_cpu_usage = 0.01
-app_mem_usage = 10
+class load:
+    net_capacity = 1000
+    app_cpu_usage = 0.01
+    app_mem_usage = 10
+    app_net_usage = 10
 
-while(True):
-    cpu_percent = psutil.cpu_percent(4)
-    cpu_usage = psutil.cpu_count() * ( (100 - cpu_percent) / 100)
-    mem_usage = psutil.virtual_memory()[1]
+    def __init__(self) -> None:
+        while(True):
+            cpu_percent = psutil.cpu_percent(4)
+            cpu_available = psutil.cpu_count() * ( (100 - cpu_percent) / 100)
+            mem_available = psutil.virtual_memory()[1]
+            
+            net_stat = psutil.net_io_counters(nowrap=True)
+            net_in_1 = net_stat.bytes_recv
+            net_out_1 = net_stat.bytes_sent
+            time.sleep(1)
+            net_stat = psutil.net_io_counters(nowrap=True)
+            net_in_2 = net_stat.bytes_recv
+            net_out_2 = net_stat.bytes_sent
 
-    app_count = round(min(mem_usage / app_mem_usage, cpu_usage / app_cpu_usage))
+            net_in = round((net_in_2 - net_in_1) / 1024, 3)
+            net_out = round((net_out_2 - net_out_1) / 1024, 3)
+            net_available = self.net_capacity - (net_in + net_out)
 
-    print(app_count)
+            app_count = round(min(mem_available / self.app_mem_usage, cpu_available / self.app_cpu_usage, net_available / self.app_net_usage))
 
-    f = open("stat.html", 'w')
-    f.writelines(str(app_count))
-    f.close()
-    
-    time.sleep(10)
+            print(app_count)
+
+            f = open("stat.html", 'w')
+            f.writelines(str(app_count))
+            f.close()
+            
+            time.sleep(10)
+
+if(__name__ == "__main__"):
+    load()
